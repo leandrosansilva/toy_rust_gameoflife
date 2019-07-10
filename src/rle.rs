f7d71d886089869855b557ab67dda3deb4eb60bc
Leandro Santiago <leandrosansilva@gmail.com>
2019-07-10 21:50:24 +0200

    WIP: reading cells from LRE

--- src/rle.rs
+++ src/rle.rs
@@ -19,15 +19,30 @@ x = 3, y = 3
 bo$2bo$3o!
 
 "#;
-        let parsed = parse(content).unwrap(); 
+        let parsed = parse(content).unwrap();
         assert!(parsed.x == 3);
         assert!(parsed.y == 3);
+
+        let mut storage = FakeStorage{cells: vec![]};
+        parsed.add_cells(&mut storage);
+
+        use crate::world::Coord;
+
+        assert_eq!(storage.cells, vec![
+                   Coord(1, 0),
+                   Coord(2, 1),
+                   Coord(0, 2), Coord(1, 2), Coord(2, 2)
+        ]);
     }
 
     #[test]
     fn parse_body() {
         LreFile::parse(Rule::Body, "bo$2bo$3o!").unwrap();
     }
+}
+
+pub trait LifePlaceMaker {
+    fn make_cell_alive(&mut self, coord: Coord);
 }
 
 use pest::Parser;
@@ -40,6 +55,25 @@ struct LreFile;
 struct LreLife {
     x: i32,
     y: i32,
+}
+
+impl LreLife {
+    fn add_cells(&self, storage: &mut LifePlaceMaker) {
+
+    }
+}
+
+use crate::world::Coord;
+
+#[derive(Debug)]
+struct FakeStorage {
+    cells: Vec<Coord>,
+}
+
+impl LifePlaceMaker for FakeStorage {
+    fn make_cell_alive(&mut self, coord: Coord) {
+
+    }
 }
 
 fn parse(content: &str) -> Result<LreLife, pest::error::Error<Rule>> {
@@ -60,7 +94,7 @@ fn parse(content: &str) -> Result<LreLife, pest::error::Error<Rule>> {
     }
 
     let p = LreFile::parse(Rule::File, content)?.next().unwrap();
-    
+
     let mut inner = p.into_inner();
     inner.next();
     let node = inner.next();
